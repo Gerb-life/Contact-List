37348b6ee350b13a06b29385256b0236f074ab24
aordonez2 <92597322+aordonez2@users.noreply.github.com>
2022-09-14 21:58:20 -0400

    Update PersonalContact.java

--- src/ClassFiles/PersonalContact.java
+++ src/ClassFiles/PersonalContact.java
@@ -10,6 +10,12 @@ public class PersonalContact extends Contact implements Cloneable{
         super(person, address, phone, email);
         this.label = label;
     }
+    
+    /**
+     *  Constructs a string with the current contact information such as the person's name,
+     *  last name, marital status and the person's address.
+     * @return a string with a contact information.
+     */
     public String toString() {
         return "Category: " + label + "\n\t\t" +
                 this.getPerson().last +","+ this.getPerson().first + "  Phone: " + this.getPhone()  + "\n\t\t" +
